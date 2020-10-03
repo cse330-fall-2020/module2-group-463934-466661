9d83e8ffd9fa4d8223e17f952b1aa32074a2b3dd
myrien99 <myrien@wustl.edu>
2020-10-03 11:42:21 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Mod2 _file sharing_Login </title>
+</head>
+<body>
+    <form name= "login" action= "loginVerify.php" Method="GET">  
+    
+        Enter Username: <input type="text" name="username"/>
+        <input type="submit" value="submit">
+    </form>
+    <!--php section-->
+    
+
+</body>
+
+</html>
