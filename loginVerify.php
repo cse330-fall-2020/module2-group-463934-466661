1ed66c68b80683d7777b40f59877e34909dc8981
myrien99 <myrien@wustl.edu>
2020-10-04 17:29:09 -0500

    Update loginVerify.php

--- loginVerify.php
+++ loginVerify.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <title>login Verify</title>
 </head>
@@ -10,32 +10,37 @@
     <?php
         echo "hello";
         $h = fopen("/home/nadia/Mod2Secure/users.txt", "r");
-        $user = $_GET["username"];
-        echo trim(fgets($h));
+        $user = $_POST['username'];
+        /// create new user
+        
+        echo nl2br(", username not found. Go back and re-enter valid username. 
+        \nWould you like to create a new user for".' '.$user.'?');
+
+        printf('<form action= addNewUser.php Method="POST">
+            <input type= "hidden" name= "newUser" value= "%s"> 
+            <input type= "submit" value="create new user">
+        </form>',$user);
+        
+        
 
         while( !feof($h) ){
             
-            if( $user== trim(fgets($h))){ ///trim to exclude new line character
-                echo "hello";
+            if( $user== trim(fgets($h)) &&  !empty($_POST["username"]) ){ ///trim to exclude new line character
+                
                 //enter session for specific user
                 session_start();
                 $_SESSION['username'] = $user; // bc enter if state, get sessioin that equals username
                 // in session phps that we want to use user name $user=session[username]
                 // here were setting it other phps we get it
                 
-
                  header("Location: userFilesActions.php");
                  exit;
-
-
 
             }
             
         }
         fclose($h);
     ?>
-        
- 
 
 </body>
 
