583f2203b94b928cf1d2ba9d2358d5f86178aac0
myrien99 <myrien@wustl.edu>
2020-10-04 17:27:00 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <title> Mod2 _file sharing_Login </title>
 </head>
 <body>
-    <form name= "login" action= "loginVerify.php" Method="GET">  
+    
+    <form name= "login" action= "loginVerify.php" Method="POST">  
     
         Enter Username: <input type="text" name="username"/>
         <input type="submit" value="submit">
     </form>
     <!--php section-->
-    
+    <?php
+        session_start();
+        echo $_SESSION['username'];
+    ?>
 
 </body>
 
