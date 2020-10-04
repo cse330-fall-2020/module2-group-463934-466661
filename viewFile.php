8189a9f6e8c8e4191995b5adfe4358a71a459087
myrien99 <myrien@wustl.edu>
2020-10-04 17:38:19 -0500

    Update viewFile.php

--- viewFile.php
+++ viewFile.php
@@ -1,23 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title> Mod 2 view file</title>
-</head>
+<?php
+    session_start();
+    $user= $_SESSION['username'];
+    //user path to user file, to get file 
+    $filename = $_POST['view'];
+    
+    $full_path = sprintf("/home/nadia/Mod2Secure/%s/%s", $user, $filename);
+    
 
-<body>
-    <?php
-        session_start()
-        $user= $_SESSION['username']   
-        $filename = $_GET['fileName'];
-        $full_path = sprintf("/srv/uploads/%s/%s", $username, $filename);
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    $mime = $finfo->file($full_path);
+    
+    header("Content-Type: ".$mime);
+    
+    header('content-disposition: inline; filename="'.$filename.'";');
+    
+    readfile($full_path);
+    
+?> 
 
-        $finfo = new finfo(FILEINFO_MIME_TYPE);
-        $mime = $finfo->file($full_path);
-        
-        header("Content-Type: ".$mime);
-        header('content-disposition: inline; filename="'.$filename.'";');
-        readfile($full_path);
-    ?> 
-</body>
-
-</html>
