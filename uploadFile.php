99c05ff9282fa6e9e2621cf51d220e7ea1c5467f
myrien99 <myrien@wustl.edu>
2020-10-04 17:35:23 -0500

    Update uploadFile.php

--- uploadFile.php
+++ uploadFile.php
@@ -7,14 +7,38 @@
 <body>
    
     <?php
-        session_start()
-        $user = $_SESSION['username']   
-        $file = $_FILES['uploadedfile']['name']// from userFiles.php upload form
+        session_start();
+        $user = $_SESSION['username'];   
+        $file = $_FILES['uploadedfile']['name'];// from userFilesACtion.php upload form
+        
 
-        $full_path = sprintf("/srv/uploads/%s/%s", $user, $file); //Q: to I have to change to path 
-        move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $full_path)
+        $directoryfullpath =sprintf("/home/nadia/Mod2Secure/%s", $user); // creates directory if dir doesnt exist yet
+        if (!file_exists($directoryfullpath) && !is_dir($directoryfullpath) ) {
+            mkdir($directoryfullpath);
+        
+        }
 
-        echo
+        
+        
+        $directory= opendir($directoryfullpath);  //opens users directory
+
+        $full_path = sprintf("/home/nadia/Mod2Secure/%s/%s", $user, $file); // $user and $file makes new  
+        move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $full_path);
+
+        
+
+        //add file to uploads.txt in Mod2Secure folder
+        
+
+        
+        echo $full_path;
+        header("Location: userFilesActions.php");
+        exit;
+
+        
+
+        // upload file gets put in temp php file system. line 12 _Files gets file name--> line 14 dictate where I want the file to be (customize path)
+        //line 15 move uploaded file tmp--> instance  
         
     ?> 
 
